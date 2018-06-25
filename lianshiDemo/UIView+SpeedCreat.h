215d121ab601ef487fba61c1b3fc91d90972ee11
shutup <shutup@shutupdeiMac.local>
2018-06-25 11:18:20 +0800

    first commit

---
+++ lianshiDemo/UIView+SpeedCreat.h
@@ -0,0 +1,13 @@
+//
+//  UIView+SpeedCreat.h
+//  lianshiDemo
+//
+//  Created by shutup on 2018/6/25.
+//  Copyright © 2018年 shutup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (SpeedCreat)
+
+@end
---
+++ lianshiDemo/UIView+SpeedCreat.m
@@ -0,0 +1,13 @@
+//
+//  UIView+SpeedCreat.m
+//  lianshiDemo
+//
+//  Created by shutup on 2018/6/25.
+//  Copyright © 2018年 shutup. All rights reserved.
+//
+
+#import "UIView+SpeedCreat.h"
+
+@implementation UIView (SpeedCreat)
+
+@end
